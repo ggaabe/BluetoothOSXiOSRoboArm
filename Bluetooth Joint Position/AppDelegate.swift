10d688d477d9e773c4b2b79dc4eddc94dedae465
ggaabe <ggaabe@gmail.com>
2016-04-18 21:08:49 -0400

    Initial Commit

---
+++ Bluetooth Joint Position/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Bluetooth Joint Position
+//
+//  Created by Gabe Garrett on 4/18/16.
+//  Copyright © 2016 Gabe. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
